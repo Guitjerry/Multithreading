12480a8df33091339ea1fe5c4dd2acd7bdffe00d
jiangbin <563292236@qq.com>
2020-04-09 16:15:40 +0800

    不要用stop来停止一个线程

---
+++ multithread-demo/src/main/java/com/threadinfo/multithread/MultithreadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.threadinfo.multithread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultithreadDemoApplication.class, args);
+    }
+
+}
