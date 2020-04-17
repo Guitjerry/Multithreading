e3e14555ae6d4c31d5bb20590733031ffd52f96b
jiangbin <563292236@qq.com>
2020-04-17 17:24:18 +0800

    CAS原理解析

---
+++ multithread-demo/src/main/java/com/threadinfo/multithread/demo7/Case.java
@@ -0,0 +1,13 @@
+package com.threadinfo.multithread.demo7;
+
+public class Case {
+    public volatile int n;
+
+    public void add() {
+        n++;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
