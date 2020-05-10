6be9de4ababcdbd77af736bbdb1b76aa8bd88bfb
jsd12060123@163.com <jerry>
2020-05-10 21:39:46 +0800

    算法

---
+++ multithread-demo/src/main/java/com/algorithm/demo/demo1.java
@@ -0,0 +1,13 @@
+package com.algorithm.demo;
+
+/**
+ * 动态规划算法
+ * 需求:你有三枚硬币，分别为2元，5元，7元，每种硬币都有足够多
+ * 买一本书需要27元
+ * 如何用最少的硬币组合正好付清，不需对方再找
+ */
+public class demo1 {
+    public static void main(String[] args) {
+
+    }
+}
