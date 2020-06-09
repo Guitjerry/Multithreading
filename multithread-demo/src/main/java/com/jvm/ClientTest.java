20a461aa9a9ccb8fc5d787feb2de0bad36987a32
jsd12060123@163.com <jerry>
2020-06-09 23:48:22 +0800

    jvm加载

---
+++ multithread-demo/src/main/java/com/jvm/ClientTest.java
@@ -0,0 +1,9 @@
+package com.jvm;
+
+public class ClientTest {
+    private int  a = 1;
+    private static  int c=3;
+    public static void main(String[] args) {
+        int b=2;
+    }
+}
--- multithread-demo/src/main/java/com/jvm/JvmTest.java
+++ multithread-demo/src/main/java/com/jvm/JvmTest.java
@@ -2,6 +2,8 @@ package com.jvm;
 
 public class JvmTest {
     public static void main(String[] args) {
-        int i =2 +3;
+        int i =2;
+        int j=3;
+        int k= i + j;
     }
 }
